6008f698384ac3a68e33daf32e382bfeac151fb1
melpin <hwangju7476@naver.com>
2021-05-24 17:39:45 +0900

    +google map add

--- happyhouse_vue/src/components/items/googlemap.vue
+++ happyhouse_vue/src/components/items/googlemap.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="app">
-    <button @click="addMarkers">add</button>
+  <div id="googlemap">
     <button @click="clearMarkers">clear</button>
-    <button @click="setCenter">move center</button>
-    <GmapMap ref="mapRef" :center="center" :zoom="16" style="width: 100vw; height: 100vh">
+    <GmapMap ref="mapRef" :center="center" :zoom="zoom" style="width: 70vw; height: 70vh">
       <GmapMarker
         :key="index"
         v-for="(m, index) in markers"
@@ -23,40 +21,17 @@
 </template>
 
 <script>
-import shuffle from "lodash/shuffle";
 import take from "lodash/take";
-const sample = [
-  { lat: 37.5047592, lng: 127.0415586, price: "₩52" },
-  { lat: 37.5082055, lng: 127.0363408, price: "₩66" },
-  { lat: 37.5055726, lng: 127.0294372, price: "₩40" },
-  { lat: 37.4994, lng: 127.03545, price: "₩39" },
-  { lat: 37.4916279, lng: 127.0289673, price: "₩43" },
-  { lat: 37.49479, lng: 127.03665, price: "₩54" },
-  { lat: 37.5052889, lng: 127.0258825, price: "₩50" },
-  { lat: 37.503028, lng: 127.0237718, price: "₩47" },
-  { lat: 37.506151, lng: 127.028389, price: "₩50" },
-  { lat: 37.505394, lng: 127.028807, price: "₩16" },
-  { lat: 37.4918215, lng: 127.0299, price: "₩38" },
-  { lat: 37.504824, lng: 127.028217, price: "₩195" },
-  { lat: 37.5012203, lng: 127.035459, price: "₩55" },
-  { lat: 37.49869, lng: 127.0323734, price: "₩51" },
-  { lat: 37.5028748, lng: 127.0394336, price: "₩32" },
-  { lat: 37.5065218, lng: 127.0303014, price: "₩41" },
-  { lat: 37.4935486, lng: 127.0280787, price: "₩49" },
-  { lat: 37.4995308, lng: 127.0354614, price: "₩28" },
-  { lat: 37.50664, lng: 127.03158, price: "₩64" },
-  { lat: 37.5024767, lng: 127.0399139, price: "₩49" },
-  { lat: 37.5013577, lng: 127.0357776, price: "₩40" },
-];
+
 export default {
-  name: "App",
+  name: "GoogleMap",
   data() {
     return {
       center: {
         lat: 37.500131499999995,
         lng: 127.03242579999998,
       },
-      data: sample,
+      zoom: 15,
       markers: [],
       infovalue: {
         content: "",
@@ -64,39 +39,52 @@ export default {
       },
     };
   },
-  computed: {},
-  watch: {},
-  methods: {
-    // 마커 추가
-    addMarkers() {
-      take(shuffle(this.data), 20).map(({ price, lat, lng }, i) => {
-        var mar = {
-          position: {
-            lat: lat,
-            lng: lng,
-          },
-          price: price + i * 100,
-        };
-        this.markers.push(mar);
-      });
+  computed: {
+    data() {
+      return this.$store.state.results;
     },
+  },
+  watch: {
+    data() {
+      // 마커 추가
+      if (this.data.length > 0) {
+        this.clearMarkers();
+        this.setCenter({
+          lat: parseFloat(this.data[0].lat),
+          lng: parseFloat(this.data[0].lng),
+        });
+        take(this.data, this.data.length).map(({ aptname, lat, lng }) => {
+          var mar = {
+            position: {
+              lat: parseFloat(lat),
+              lng: parseFloat(lng),
+            },
+            aptname: aptname,
+          };
+
+          this.markers.push(mar);
+        });
+      }
+    },
+  },
+  methods: {
     markerclick(item) {
       console.log("markerclick");
-      this.infovalue.content = item.price; // content
+      this.infovalue.content = item.aptname;
       let position = {
-        lat: item.position.lat * 1 + 0.0005,
-        lng: item.position.lng * 1,
+        lat: item.position.lat,
+        lng: item.position.lng,
       };
       this.setCenter(position);
       this.infovalue.position = position;
       this.infoWinOpen = true;
-      console.log(this.infoWinOpen);
     },
     clearMarkers() {
       this.markers = [];
     },
     setCenter(position) {
       this.center = position;
+      this.zoom = 18;
     },
   },
 };
