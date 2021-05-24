c0ce1b403a96814a24d16acaf561341f1c6294b8
melpin <hwangju7476@naver.com>
2021-05-24 09:48:05 +0900

    +

--- happyhouse_vue/src/util/http-common.js
+++ happyhouse_vue/src/util/http-common.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  // baseURL: 'http://localhost:9999/happyhouse',
-  baseURL: 'http://172.16.101.99:9999/happyhouse',
+  baseURL: "http://localhost:9999/happyhouse",
+  //baseURL: 'http://172.16.101.99:9999/happyhouse',
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
