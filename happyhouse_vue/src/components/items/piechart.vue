3e12ed70d1714fb7b6ac4f009c873ce5e5979cac
haejin <j__0308@naver.com>
2021-05-26 15:02:09 +0900

    ,

--- happyhouse_vue/src/components/items/piechart.vue
+++ happyhouse_vue/src/components/items/piechart.vue
@@ -4,22 +4,22 @@
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-import bb, { pie } from 'billboard.js';
+import { mapGetters } from "vuex";
+import bb, { pie } from "billboard.js";
 
 export default {
-  name: 'piechart1',
+  name: "piechart1",
   data() {
     return {
       chart: null,
       datalist: [
-        ['남자', 0],
-        ['여자', 0],
+        ["남자", 0],
+        ["여자", 0],
       ],
     };
   },
   computed: {
-    ...mapGetters(['statics']),
+    ...mapGetters(["statics"]),
   },
   watch: {
     statics() {
@@ -31,13 +31,13 @@ export default {
           },
           data: {
             columns: [
-              ['남자', this.statics.manusercount],
-              ['여자', this.statics.womanusercount],
+              ["남자", this.statics.manusercount],
+              ["여자", this.statics.womanusercount],
             ],
             type: pie(),
           },
           legend: {
-            position: 'right',
+            position: "right",
             equally: true,
             item: {
               tile: {
@@ -47,15 +47,15 @@ export default {
             },
           },
           color: {
-            pattern: ['#1f77b4', '#aec7e8'], // color code
+            pattern: ["#1f77b4", "#aec7e8"], // color code
           },
-          bindto: '#pieChart',
+          bindto: "#pieChart",
         });
       } else {
         this.chart.load({
           columns: [
-            ['남자', this.statics.manusercount],
-            ['여자', this.statics.womanusercount],
+            ["남자", this.statics.manusercount],
+            ["여자", this.statics.womanusercount],
           ],
         });
       }
@@ -67,5 +67,8 @@ export default {
 <style>
 .bb-legend-item > text {
   font-size: 20px;
+}
+.bb svg {
+  margin: 0 auto;
 }
 </style>
--- happyhouse_vue/src/views/dashBoard.vue
+++ happyhouse_vue/src/views/dashBoard.vue
@@ -3,11 +3,11 @@
     <div class="space"><span>Static DashBoard</span></div>
 
     <div class="blockclass">
-      <pie-chart></pie-chart>
-      <div class="text">
+      <pie-chart class="pie"></pie-chart>
+      <!-- <div class="text">
         <div>남자: {{ statics.manusercount }}명</div>
         <div>여자: {{ statics.womanusercount }}명</div>
-      </div>
+      </div> -->
     </div>
     <div class="blockclass">
       <line-chart></line-chart>
@@ -18,37 +18,30 @@
   </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-import PieChart from '@/components/items/piechart.vue';
-import LineChart from '@/components/items/linechart.vue';
+import { mapGetters } from "vuex";
+import PieChart from "@/components/items/piechart.vue";
+import LineChart from "@/components/items/linechart.vue";
 
 export default {
-  name: 'dashboard',
+  name: "dashboard",
   components: {
     PieChart,
     LineChart,
   },
   computed: {
-    ...mapGetters(['statics']),
+    ...mapGetters(["statics"]),
   },
   created() {
-    this.$store.dispatch('loadstatics');
+    this.$store.dispatch("loadstatics");
   },
 };
 </script>
 <style scoped>
-pie-chart,
-line-chart {
-  width: 50%;
-}
-.text {
-  width: 50%;
-}
-
 .blockclass {
   display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
 .space {
   display: flex;
   justify-content: center;
@@ -57,5 +50,8 @@ line-chart {
   height: 100px;
   margin: 0 auto;
   border-bottom: 1px solid black;
+}
+.pie {
+  width: 100%;
 }
 </style>
